7395eedf7c0b6b92fdd830307d1b2032b3d4b374
BRYAN <bryandavidmarcial@gmail.com>
2024-06-11 21:20:30 -0600

    intentando crear rama

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout<<"probando git";
+    cout<<"intentando crear rama";
+
     return 0;
 }
